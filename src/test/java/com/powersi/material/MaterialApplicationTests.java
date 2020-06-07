2b8dd3679924263c120d5c09a697f0b41377d14b
dxiaoping <1693547683@qq.com>
2020-06-07 22:10:15 +0800

    init

---
+++ src/test/java/com/powersi/material/MaterialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.powersi.material;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaterialApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
