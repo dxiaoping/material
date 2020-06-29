62b29b15bfb75452acc62041cad808c364f85de7
dxiaoping <1693547683@qq.com>
2020-06-29 08:42:34 +0800

    销售模块完善

--- src/main/java/com/powersi/material/service/IItemSaleService.java
+++ src/main/java/com/powersi/material/service/IItemSaleService.java
@@ -5,6 +5,7 @@ package com.powersi.material.service;
  * @mailbox: xiaoping.duan@powersi.com.cn
  * @create: 2020-06-9 10:34
  */
+import com.powersi.material.pojo.Item;
 import com.powersi.material.pojo.ItemSaleDetail;
 import com.powersi.material.pojo.requestBody.SaleListReq;
 import com.powersi.material.pojo.requestBody.SaleSettlementReq;
@@ -15,4 +16,7 @@ public interface IItemSaleService {
     List getSaleList(SaleListReq saleListReq);
     int createSaleOrder(SaleSettlementReq saleSettlementReq);
     List getSaleDetail(String itemSaleId);
+    int backSale(String id);
+    List<Item> getAllItem();
+    int createSaleOrderData(SaleSettlementReq saleSettlementReq);
 }
